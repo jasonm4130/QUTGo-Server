03553a99ea6430c3144c639a3a90066d1594f20c
camcrosse <cam.crosse97@outlook.com>
2018-10-13 16:18:17 +1000

    Week11Sat Changes
    Typo fix in removeFriend.php

--- QUTGo/removeFriend.php
+++ QUTGo/removeFriend.php
@@ -3,7 +3,7 @@
 * Author: Luke Goeree (26.09.2018)
 *
 * Require database connection and timezone setup code
-* 
+*
 * @define (MySQLi_Connect) $connect - A MySQLi_Connect variable containing the database connection information
 */
 require 'setup.php';
@@ -19,42 +19,42 @@ if(isset($_GET['method']) && !empty($_GET['method']) && isset($_GET['userid']) &
 mysqli_close($connect);
 /**
 * removeFriend
-* 
+*
 * Returns a JSON object containing a success/failure notification if the friendship removal request has been sent
-* 
+*
 * @param (MySQLi_Connect) $connect - MySQLi_Connect variable containing the database connection information
 * @param (Integer) $useridone - Integer variable containing the first user's ID
 * @param (Integer) $useridtwo - Integer variable containing the second user's ID
-* 
+*
 * @return (JSON) $users - JSON encoded String variable containing a success/failure message
 */
-function removeFriend($connect, $useridone, $useridtwo){	
+function removeFriend($connect, $useridone, $useridtwo){
 	/*
 	SQL Query for removing friendship lines from relationship database
 	Removes friendships regardless of who is listed as user_one and user_two in the database
-	
+
 		DELETE FROM relationship
 		WHERE (relationship.user_one = '$useridone' AND relationship.user_two = '$useridtwo')
 			OR (relationship.user_one = '$useridtwo' AND relationship.user_two = '$useridone')
 	*/
-	$sql = "DELETE FROM friendship WHERE (relationship.user_one = '$useridone' AND relationship.user_two = '$useridtwo') OR (relationship.user_one = '$useridtwo' AND relationship.user_two = '$useridone')";
-	
+	$sql = "DELETE FROM relationship WHERE (relationship.user_one = '$useridone' AND relationship.user_two = '$useridtwo') OR (relationship.user_one = '$useridtwo' AND relationship.user_two = '$useridone')";
+
 	/* Run the query */
 	$result = mysqli_query($connect, $sql);
-	
+
 	/* Chect the number of modified rows */
 	$rows = mysqli_affected_rows($connect);
-	
+
 	/* Set the result */
 	if($rows == 1){
 		$success = "Friend removed successfully";
 	} else {
 		$success = "Error friend not removed";
 	}
-	
+
 	/* Encode array as json */
 	$success = json_encode($success);
-	
+
 	/* Return json */
 	echo '{"result":' . $success . '}';
 }
