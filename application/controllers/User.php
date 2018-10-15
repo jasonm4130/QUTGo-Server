377e64b3fd21a7c4a19d2211be1086d4ec285bca
jasonm4130 <jasonm4130@gmail.com>
2018-10-15 14:27:16 +1000

    Change Redirect after Registration

--- application/controllers/User.php
+++ application/controllers/User.php
@@ -28,7 +28,7 @@ class User extends CI_Controller {
         if($email_check){
             $this->user_model->register_user($user);
             $this->session->set_flashdata('success_msg', 'Registered successfully.Now login to your account.');
-            redirect('user/login_view');
+            redirect(base_url());
         }
         else{
             $this->session->set_flashdata('error_msg', 'That Email Address is already Registered');
